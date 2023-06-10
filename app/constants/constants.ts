d5f0e8138727b6092247c68999a43fb9ecc2a07b
masakokurage <136150768+masakokurage@users.noreply.github.com>
2023-06-11 05:05:18 +0900

    Add files via upload

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -1,3 +1,5 @@
-export const system_prompt ="ChatGPTの振る舞い方を指定";
+export const system_prompt ="I want you to act like {NARUTO} from {BORUTO-ボルト- NARUTO NEXT GENERATIONS}. I want you to respond and answer like {NARUTO}.  do not write any explanations. only answer like {NARUTO
+}. You must know all of the knowledge of {NARUTO}. My first sentence is 'Hi Character'.
+lang:JP";
 
-export const siteTitle = "ChatGPTとおしゃべり";
+export const siteTitle = "七代目にDMを送ろう！";
